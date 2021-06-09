8516bef27be8903ff1e37214994867b8a0fe1709
PhillyJB <philipbuuza@gmail.com>
2021-06-09 20:15:48 +0000

    added js code to render markers and cluster markers on the google map

--- js/script.js
+++ js/script.js
@@ -1,19 +1,39 @@
 //Google maps JS to render our Google Maps onto the page:
 // Initialize and add the map
-function initMap() {
+function initMap() { 
     // The location of London
-    var london = { lat: 51.509865, lng: -0.118092};
+    var newcastle = { lat: 54.9783, lng: 1.6178};
+    
     // The map, centered at London
     var googleMap = new google.maps.Map(document.getElementById("google-map"), {
-        zoom: 5,
-        center: london,
+        zoom: 4.8,
+        center: newcastle,
     });
-    // The marker, positioned at London
-    var londonMarker = new google.maps.Marker({
-        position: london,
-        map: googleMap,
-        /*this value is googleMap as in the variable defined above*/
+    
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+            {lat: 55.6090, lng: 1.7099},
+            {lat: 53.7875, lng: -3.0533},
+            {lat: 53.5639, lng: 3.1003},
+            {lat: 54.4903, lng: 3.6062},
+            {lat: 53.3723, lng: 3.1902},
+            {lat: 54.4911, lng: 0.6162},
+    ];
+
+    var markers = locations.map(function(location, i){
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length],
+        });
+
     });
+
+    var markerCluster = new MarkerClusterer(map, markers, {
+        imagePath:
+          "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+      });
+
 }
 
 //JS to hide and toggle on beach information
